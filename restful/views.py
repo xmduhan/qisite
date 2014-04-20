958b1013ff1f701edac231ad8c1033a9ae2a1aab
xmduhan <xmduhan@gmail.com>
2014-04-20 11:11:29 +0800

    添加总体ER设计

--- restful/views.py
+++ restful/views.py
@@ -1,28 +1,32 @@
 # coding=utf-8
 # Create your views here.
 
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from rest_framework import routers
 from rest_framework.viewsets import ModelViewSet
-import home
+from rest_framework import status
 
 
 def prferMedia(http_accept):
     return http_accept.split(',')[0]
 
+
 def isJsonMedia(request):
     http_accept = request.META['HTTP_ACCEPT']
-    if prferMedia(http_accept) in ['application/json'] : # ,'text/html']:
+    if prferMedia(http_accept) in ['application/json']:
+    #if prferMedia(http_accept) in ['application/json','text/html']:
         return True
     else:
         return False
 
+
 # 重载ModelViewSet屏蔽json以外的访问方式
-class JsonModelViewSet(ModelViewSet):
-    errorResponse = HttpResponse("{'status': 'only json accept'}")
-    #def __init__(self,**kwargs):
-    #    return super(ModelViewSet, self).__init__(**kwargs)
+class QiModelViewSet(ModelViewSet):
+
+    def __init__(self,**kwargs):
+        self.errorResponse = HttpResponse("{'status': 'only json accept'}")
+        super(ModelViewSet, self).__init__(**kwargs)
 
     def list(self, request):
         if (isJsonMedia(request)):
@@ -61,21 +65,11 @@ class JsonModelViewSet(ModelViewSet):
             return self.errorResponse
 
 
-# ViewSets define the view behavior.
-class UserViewSet(JsonModelViewSet):
+# 定义需要提供restful服务的数据模型的代理类
+class UserViewSet(QiModelViewSet):
     model = User
 
 
-class GroupViewSet(ModelViewSet):
-    model = Group
-
-
-class CatalogViewSet(ModelViewSet):
-    model = home.models.Catalog
-
-# Routers provide an easy way of automatically determining the URL conf.
-
+# 设置restful的url和代理类的关系
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'catalog', CatalogViewSet)
