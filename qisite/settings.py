3eb3b44374da0d909ebbff408dbb237f2414cf59
xmduhan <xmduhan@gmail.com>
2014-05-25 15:10:43 +0800

    初步完成登录功能的开发。

--- qisite/settings.py
+++ qisite/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 import south
 
@@ -46,7 +47,6 @@ INSTALLED_APPS = (
     'account',
     'demos',
 )
-
 
 TEMPLATE_DIRS = (
 )
@@ -93,6 +93,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 
 # Configuration for django-rest-framework
 REST_FRAMEWORK = {
--- www/views.py
+++ www/views.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 # Create your views here.
-from django.template import loader,Context,RequestContext
+from django.template import loader, Context, RequestContext
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 
 
 def index(request):
     template = loader.get_template('www/index.html')
-    context = RequestContext(request)
+    context = RequestContext(request, {'session': request.session})
+    print context
     return HttpResponse(template.render(context))
+
 
 def help(request):
     template = loader.get_template('www/help.html')
-    context = RequestContext(request)
+    context = RequestContext(request, {'session': request.session})
     return HttpResponse(template.render(context))
