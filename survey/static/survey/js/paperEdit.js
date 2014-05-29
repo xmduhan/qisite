2abe3d32fc985d8ed3a88e4befa4f59692cdefa9
xmduhan <xmduhan@gmail.com>
2014-05-29 17:23:10 +0800

    ...

--- survey/static/survey/js/paperEdit.js
+++ survey/static/survey/js/paperEdit.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $(".bootstrap-switch").bootstrapSwitch.defaults.onText = '是';
     $(".bootstrap-switch").bootstrapSwitch.defaults.offText = '否';
     $(".bootstrap-switch").bootstrapSwitch();
+    $(".bootstrap-switch-confused").bootstrapSwitch.defaults.onText = '乱序';
+    $(".bootstrap-switch-confused").bootstrapSwitch.defaults.offText = '正常';
+    $(".bootstrap-switch-confused").bootstrapSwitch();
 });
