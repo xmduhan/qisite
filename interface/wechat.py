8b511e9d35960d84fa9965899d4e5e21f1082a1f
xmduhan <xmduhan@gmail.com>
2014-06-21 20:26:35 +0800

    增加微信的接受接口测试(解决csrf豁免问题)

--- interface/wechat.py
+++ interface/wechat.py
@@ -1,8 +1,10 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def service(request):
     print request.REQUEST
     return HttpResponse('')
