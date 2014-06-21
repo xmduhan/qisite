3c5624274cf7b6730c03f75342c96ebc5d93c428
xmduhan <xmduhan@gmail.com>
2014-06-21 20:57:31 +0800

    ...

--- interface/wechat.py
+++ interface/wechat.py
@@ -10,9 +10,12 @@ def service(request):
     '''
         微信服务的中转器
     '''
+    #request = HttpRequest()
     print '------1--------'
     print request.REQUEST
     print '------2--------'
     print request.POST
     print '------3--------'
+    print request.body
+    print '------4--------'
     return HttpResponse('ok')
