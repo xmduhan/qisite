cc77f20aa7df5137073a54a702692e930421b844
xmduhan <xmduhan@gmail.com>
2014-06-21 20:39:03 +0800

    ...

--- interface/wechat.py
+++ interface/wechat.py
@@ -6,6 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def service(request):
+    '''
+        微信服务的中转器
+    '''
     print request.REQUEST
+    print request.raw_post_data
     return HttpResponse('')
 
