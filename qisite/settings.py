c5c8e0fb8d3e88282186ae48b7b4d5afd15ee0df
xmduhan <xmduhan@gmail.com>
2014-06-23 12:49:42 +0800

    增加STATIC_ROOT配置

--- qisite/settings.py
+++ qisite/settings.py
@@ -95,6 +95,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/qi/static'
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
