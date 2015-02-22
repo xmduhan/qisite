8ffc8321d89259a2ebe63f94b3d32294bb98ef86
xmduhan <xmduhan@gmail.com>
2015-02-22 17:44:04 +0800

    ... ...

--- qisite/settings.py
+++ qisite/settings.py
@@ -91,7 +91,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = config.smsSend
+STATIC_ROOT = config.STATIC_ROOT
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
