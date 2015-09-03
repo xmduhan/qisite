71880998b360caacf7e800f200dd6ca930aa99b3
xmduhan <xmduhan@gmail.com>
2015-09-03 18:50:55 +0800

    ...

--- survey/static/survey/js/surveyViewResult.js
+++ survey/static/survey/js/surveyViewResult.js
@@ -16,22 +16,6 @@ pieOpition = {
 }
 
 /***************************************
- *          柱状图的参数定义           *
- ***************************************/
-barOpition = {
-    seriesDefaults: {
-        // Make this a pie chart.
-        renderer: jQuery.jqplot.PieRenderer,
-        rendererOptions: {
-            // Put data labels on the pie slices.
-            // By default, labels show the percentage of the slice.
-            showDataLabels: true
-        }
-    },
-    legend: { show: true, location: 'e' },
-    grid: {shadow: false, borderWidth: 0}
-}
-/***************************************
  *          展现一个饼图               *
  ***************************************/
 
@@ -46,35 +30,12 @@ function renderPieChart(chartId) {
 
 function renderBarChart(chartId) {
     data = eval($("#" + chartId).data('chart-data'));
-    jQuery.jqplot(chartId, [data], barOpition);
-}
+    //var ticks = ['a', 'b', 'c', 'd'];
+    //var s1 = [2, 6, 7, 10];
+    var ticks = data[0]
+    var s1 = data[1]
 
-/***************************************
- *          展现所有的图表             *
- ***************************************/
-
-function renderAllChart() {
-
-    $(".jqplot-pie-chart").each(function () {
-        renderPieChart($(this).attr('id'));
-    })
-
-    //$(".jqplot-bar-chart").each(function () {
-    //    renderBarChart($(this).attr('id'));
-    //})
-
-}
-
-/***************************************
- *          全局初始化函数             *
- ***************************************/
-
-function test01(){
-    $.jqplot.config.enablePlugins = true;
-    var s1 = [2, 6, 7, 10];
-    var ticks = ['a', 'b', 'c', 'd'];
-
-    plot1 = $.jqplot('chart3', [s1], {
+    plot1 = $.jqplot(chartId, [s1], {
         animate: false,
         seriesDefaults:{
             renderer:$.jqplot.BarRenderer,
@@ -88,12 +49,28 @@ function test01(){
         },
         highlighter: { show: false }
     });
+}
+
+/***************************************
+ *          展现所有的图表             *
+ ***************************************/
+
+function renderAllChart() {
+
+    //$(".jqplot-pie-chart").each(function () {
+    //    renderPieChart($(this).attr('id'));
+    //})
+
+    $(".jqplot-bar-chart").each(function () {
+        renderBarChart($(this).attr('id'));
+    })
 
 }
 
-
-
+/***************************************
+ *          全局初始化函数             *
+ ***************************************/
 $(document).ready(function () {
+    $.jqplot.config.enablePlugins = true;
     renderAllChart();
-    //test01();
 });
